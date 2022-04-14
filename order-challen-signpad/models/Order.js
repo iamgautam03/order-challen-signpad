3fdf71992b5ea460243f7968fcab31461d27edd8
jaykradadiya <jaykradadiya2001@gmail.com>
2022-04-14 11:46:29 +0530

    Added models

---
+++ order-challen-signpad/models/Order.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const OrderSChema = new Schema({
+    OrderId:{
+        type:String,
+        required:true
+    },
+    OrderDate:{
+        type:Date,
+        required:true
+    },
+    OrderTotal:{
+        type:Number,
+        required:true
+    },
+    ReceiverName:{
+        type:String,
+        required:true
+    },
+    ReceiverAddress:{
+        type:String,
+        required:true
+    },
+    ReceiverGSTIN:{
+        type:String
+    },
+    CompanyId:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:"user"
+    },
+    GeneratedLink:{
+        type:String
+    },
+    SignStatus:{
+        type:Boolean,
+        default:false
+    },
+    SignData:{
+        type:Buffer
+    },
+    SignDataContentType:{
+        type:String
+    }
+},{
+    timestamp:true
+})
+
+
+const Order = mongoose.model("Order",OrderSChema)
+
+module.exports = Order;
