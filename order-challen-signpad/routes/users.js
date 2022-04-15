f2ca0a2c04318d5df170825f38ffd8882a4432cf
Gautam Patel <62021436+iamgautam03@users.noreply.github.com>
2022-04-15 15:21:43 +0530

    added-add-order-functionality
    - add order backend
    - get business details through company id
    - generate token and send back

--- order-challen-signpad/routes/users.js
+++ order-challen-signpad/routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
+const User = require('../models/User');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:CompanyId', function(req, res, next) {
+  User.findOne({"CompanyId":req.params.CompanyId})
+    .then((user) => {
+        if(!user) {
+            res.statusCode=404;
+            res.setHeader('content-type','application/json');
+            res.send({
+                "error":"Data not found"
+            });
+        }
+        res.statusCode=200;
+        res.setHeader('content-type','application/json');
+        res.send({
+          businessName: user.BusinessName,
+          businessAddress: user.BusinessAddress,
+          businessGSTIN: user.GSTIN,
+        });
+    })
+    .catch(err=>next(err));
 });
 
 module.exports = router;
