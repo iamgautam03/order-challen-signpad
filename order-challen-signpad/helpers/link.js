c826dee7b09ef411d3253f21ca337a8ea8f8cba5
jaykradadiya <jaykradadiya2001@gmail.com>
2022-04-15 15:20:45 +0530

    added get ROute in signpad

--- order-challen-signpad/helpers/link.js
+++ order-challen-signpad/helpers/link.js
@@ -3,27 +3,23 @@ var ENC = require("crypto-js/enc-utf8");
 const Order = require("../models/Order");
 const SECRET = "F.2(H\Kv/MvXCR&G!!B.Zf9c";
 
-module.exports = generateLinkToken = (orderId, companyId) => {
+exports.generateLinkToken = (orderId, companyId) => {
     const tokenFor = `${orderId}+${companyId}`;
     const linkToken = AES.encrypt(tokenFor, SECRET).toString();
     return linkToken;
 }
 
-module.exports = verifyLinkToken = async (token) => {
+exports.verifyLinkToken = async (token) => {
     const bytes = AES.decrypt(token, SECRET);
     const tokenFor = bytes.toString(ENC);
     const [orderId, companyId] = tokenFor.split('+');
 
-    await Order.find({
+    return await Order.findOne({
         companyId,
         orderId
-    }).then((orders) => {
-        if (orders.length)
-            return order;
-        return null;
+    }).then((order) => {
+        return order
     }).catch((err) => {
         return null;
     })
-
-    return null;
 }
--- order-challen-signpad/routes/orders.js
+++ order-challen-signpad/routes/orders.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const orderRouter = express.Router();
-
+const { verifyLinkToken,generateLinkToken } = require('../helpers/link')
 const Order = require('../models/Order')
 orderRouter.use(express.json());
 orderRouter.use(express.urlencoded({extended:false}));
@@ -16,7 +16,6 @@ orderRouter
     })
     .catch(err=>next(err));
 })
-
 .get("/:OrderId",(req,res,next)=>{
     Order.findOne({"OrderId":req.params.OrderId})
     .then((orders)=>{
@@ -32,6 +31,26 @@ orderRouter
         res.send(orders);
     })
     .catch(err=>next(err));
+})
+.get('/token/:token',async (req,res,next)=>{
+    
+    // const token = await generateLinkToken("6246c492219d326d633df9ee","6246c492219d326d633df9ee");
+    // console.log(token);
+    const order = await verifyLinkToken(decodeURIComponent( req.params.token))
+    console.log(order);
+
+    res.setHeader('content-type','application/json');
+    if(!order){
+        res.statusCode=404;
+        res.send({
+            "error":"Data not found"
+        });
+    }
+    res.statusCode=200;    
+    res.send(order);
+    // res.statusCode=200;
+    // res.setHeader('content-type','application/json');
+    // res.send(encodeURIComponent( token));
 })
 
 module.exports= orderRouter;
